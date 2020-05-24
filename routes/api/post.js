a834f26bb7d85d38dfec469adb36db2677dbb58a
praveenvasa <46399250+praveenvasa@users.noreply.github.com>
2020-05-24 14:19:05 +0530

    Delete post.js

--- routes/api/post.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route  GET api/post
-// @desc   Test Route
-// @access Public
-router.get('/', (req, res) => res.send('Post Router'));
-
-module.exports = router;
